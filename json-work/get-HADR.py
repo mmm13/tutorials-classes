86af2934d133251aad34adc796848e95a51cff90
mmmcnul <m.mcnully13@gmail.com>
2018-01-18 08:35:55 -0600

    added iteration thru data

--- json-work/get-HADR.py
+++ json-work/get-HADR.py
@@ -1,10 +1,18 @@
 #! /usr/bin/python
 import json
-from pprint import pprint
+#from pprint import pprint
+#pprint(data)
 
 json_file='/tmp/mich.json'
-
-json_data=open(json_file)
-data=json.load(json_data)
-
-pprint(data)
+with open(json_file, "r") as jf:
+    data = json.load(jf)
+#print data["hits"]["hits"][4]["_source"]["plat"]
+# for k,v in data.items():
+#     print("K: "+ k)
+#     print(v)
+platforms=[]
+for hit in data["hits"]["hits"]:
+    #print hit["_source"]["plat"]
+    platforms.append(hit["_source"]["plat"])
+for platform in platforms:
+    print "Platform : "+ platform
